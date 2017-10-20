76b927a60d504b6d4b08889986f665e0026acc84
Pablo Sanz <pablosanzf@gmail.com>
2017-10-20 18:38:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/usuariopc/lm3_studygroup/MemberListActivity.java
@@ -0,0 +1,13 @@
+package com.example.usuariopc.lm3_studygroup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MemberListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_list);
+    }
+}
