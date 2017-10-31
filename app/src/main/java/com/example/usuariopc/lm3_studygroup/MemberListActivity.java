99ceaef3e19cf52646fc25c16d06e54e2720cf33
pablosanzf <pablosanzf@gmail.com>
2017-10-31 18:57:42 +0100

    Lab2 completed, lab 3 started

--- app/src/main/java/com/example/usuariopc/lm3_studygroup/MemberListActivity.java
+++ app/src/main/java/com/example/usuariopc/lm3_studygroup/MemberListActivity.java
@@ -4,11 +4,15 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.ListActivity;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -20,9 +24,13 @@ import java.util.ArrayList;
 
 public class MemberListActivity extends ListActivity {
 
+    static final int SELECT_CONTACT = 1;
+
     private ArrayList<Student> arrStudents;
     private ArrayAdapter<Student> adpStudents;
     private int posicionClase = new Integer(0);
+
+
 
     //Sirve comonexo de unión entre la estructura de datos y
     // el elemento de visualización. Para decidir que añadimos en la lista de los datos
@@ -53,9 +61,49 @@ public class MemberListActivity extends ListActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.mnu_add_student) {
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI); //es la URI propia de la listas de contactos
+            startActivityForResult(intent, SELECT_CONTACT); //es lanzar una actividad acon la finalidad de que haya un resultado, SELECT_CONTACT es el código de invocación propio de esta actividad
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { //aparece cada vez que hay una activity esperando un resultado
+        if (requestCode == SELECT_CONTACT) { //Comprobar que el request code es el que mandaste
+            if (resultCode == RESULT_OK) { //El usuario ha completado tod0 el proceso
+                Cursor cursor = getContentResolver().query(data.getData(), new String[]  /*
+                    El content representa el una llamada al contenido, como si una base de datos se tratase
+                    además esta consultas se hace como en SQL, Priemro la tabla que en nuestro caso es el data y luego el array
+                       de strings es las cosas que se han de seleccionar como "select __array de strings__ , from ___tabla___", ....where
+                       ese where es los valores nulls pero se podria añadir wheres añadiendo arrays de strings donde por ejemplo el número empezase por 7*/
+                        {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                                ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null);
+                /*Y esto devuelve un cursor que además está situado en el punto -1 por ello se necesita el moveNext*/
+                if(cursor.moveToNext()){
+                    Log.i("Intent:",data.getDataString()); //Log es lo que utilicamos para sacar información por log
+                    arrStudents.add(new Student(cursor.getString(0),
+                            cursor.getString(1))); //Y aquí se añaden los datos de la tabla, siendo el nombre el 0 y el número el 1
+                    adpStudents.notifyDataSetChanged(); //Método propio del adaptador que se autoreferesca
+                    return;
+                }
+            }
+        }
+        // Recommended
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
     private void createStudentList() {
         arrStudents = new ArrayList<Student>();
-        arrStudents.add(new Student("Javi Seco", "680310979"));
+        arrStudents.add(new Student("Jose Luis", "611111234"));
         arrStudents.add(new Student("Ibai", "22222222"));
         arrStudents.add(new Student("Itziar", "33333333"));
         arrStudents.add(new Student("Irene", "44444444"));
